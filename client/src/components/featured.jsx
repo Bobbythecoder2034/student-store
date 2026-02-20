a7fe5dd4eede5f445dfa7fd5f6f7280def3f1855
ryder <tootwo2to22@gmail.com>
2026-02-20 15:17:46 -0700

    push quick

--- client/src/components/featured.jsx
+++ client/src/components/featured.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Grid from './grid'
 import { Link } from 'react-router-dom'
 import { FaArrowRight } from 'react-icons/fa'
+import {NavLink} from "react-router-dom";
 
-const Featured = ({name, price, category, buzzwordOne, buzzwordTwo, order, id}) => {
+
+const Featured = ({name, price, category, buzzwordOne, buzzwordTwo, order, id, _id}) => {
   const link = name.replaceAll(' ', '-').toLowerCase()
   var position = null
   if(!order){
@@ -24,18 +26,17 @@ const Featured = ({name, price, category, buzzwordOne, buzzwordTwo, order, id}) 
   }
 
   return (
-<div className={`featured-product featured-product-${position}`}>
+<div className={`featured-product featured-product-${position} dark:bg-gray-800`}>
             <Grid />
-            <p className="featured-product-name">{name}</p>
-            <p className="featured-product-price">{price}</p>
-            <p className="featured-product-category">{category}</p>
-            <div className="buzzword buzzword-one">{buzzwordOne}</div>
-            <div className="buzzword-two buzzword">{buzzwordTwo}</div>
+            <p className="featured-product-name dark:text-white text-black">{name}</p>
+            <p className="featured-product-price dark:text-white text-gray-500">{price}</p>
+            <p className="featured-product-category dark:text-gray-400 text-gray-500">{category}</p>
+            <div className="buzzword buzzword-one dark:text-black">{buzzwordOne}</div>
             <div className="in-stock buzzword-three buzzword">In stock</div>
-            <div className="featured-details">
-              <p className="view"><Link to={`/${id}`}> View Detail</Link></p>
+            <NavLink className="featured-details" to={`/products/${_id}`}>
+              <p className="view dark:text-black"> View Detail</p>
               <FaArrowRight style={{ fontSize: "small" }} />
-            </div>
+            </NavLink>
           </div>  )
 }
 
